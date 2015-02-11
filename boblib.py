ff8ed5de77de74cb73417862408bfff13bec0c27
guddimon- <github.guddimon-@gutmann-web.net>
2015-02-11 01:40:21 +0100

    - improved reconnect feature (more robust in case of disconnects) - if "HELLO" fails, try to restart the service

--- boblib.py
+++ boblib.py
@@ -2,6 +2,7 @@ import time
 from telnetlib import Telnet
 from string import split
 from socket import error
+from subprocess import call
 
 # class Color for use in class Light (each light has it's own color)
 class Color:
@@ -111,6 +112,7 @@ class Boblight:
     _priority   = int(254)
     _brightness = 1
     
+    _timeout    = 1
     _reconnect  = False
     
     def __init__(self, host="", port=19333, priority=255):
@@ -126,29 +128,38 @@ class Boblight:
         else:
             self._host = host
             self._port = port
-            self._tn = Telnet(host, port)
-            
-            self._tn.write(self._HELLO)
-            self._tn.read_until(self._EOL)
-            
-            if not self._reconnect:
-                self._getLightsFromServer()
-            
-            self._reconnect = False
+            try:
+                hi = ""
+                while hi == "":
+                    self._tn = Telnet(host, port)
+                    
+                    self._tn.write(self._HELLO)
+                    hi = self._tn.read_until(self._EOL, self._timeout)
+                    
+                    if hi == "":
+                        self._restartBoblightDaemon()
+                    
+                if not self._reconnect:
+                    self._getLightsFromServer()
+                
+                self._reconnect = False
+            except error, e:
+                print e
     
     def _getLightsFromServer(self):
         self._tn.write(self._GETLIGHTS)
-        lights = self._tn.read_until(self._EOL)
+        lights = self._tn.read_until(self._EOL, self._timeout)
 
         size = int(lights.split(self._SPLITTER)[1])
         
         for i in range(0, size):
-            light = self._tn.read_until(self._EOL, 10)
+            light = self._tn.read_until(self._EOL, self._timeout)
             light_split = split(light, self._SPLITTER)
 
             self._light.append(Light(light_split[1], light_split[3], light_split[4], light_split[5], light_split[6]))
 
     def _sendPriority(self):
+        self.ping()
         self._tn.write(self._SETPRIORITY.format(self._priority))
         
     def setPriority(self, priority):
@@ -163,6 +174,7 @@ class Boblight:
         return priority
     
     def _sendColor(self):
+        self.ping()
         for l in self._light:
             self._tn.write(self._SETLIGHTRGB.format(l.getName(), self._brightness * l.getColor().getRed(), self._brightness * l.getColor().getGreen(), self._brightness * l.getColor().getBlue()))
             
@@ -175,6 +187,7 @@ class Boblight:
         self._sendColor()
         
     def disconnect(self):
+        self.ping()
         if self._tn:
             self._tn.close()
             self._tn = None
@@ -182,6 +195,7 @@ class Boblight:
             raise Boblight.ConnectionError("There was no connection to disconnect from!")
         
     def _sendSpeed(self):
+        self.ping()
         for l in self._light:
             self._tn.write(self._SETLIGHTSPEED.format(l.getName(), l.getSpeed()))
         
@@ -198,6 +212,7 @@ class Boblight:
         self._sendInterpolation()
         
     def _sendInterpolation(self):
+        self.ping()
         for l in self._light:
             self._tn.write(self._SETLIGHTINTERPOLATION.format(l.getName(), l.getInterpolation()))
         
@@ -213,7 +228,7 @@ class Boblight:
     def ping(self):
         try:
             self._tn.write(self._PING)
-            p = self._tn.read_until(self._EOL)
+            p = self._tn.read_until(self._EOL, self._timeout)
 
             if p == "ping 1"+self._EOL:
                 return True
@@ -222,6 +237,7 @@ class Boblight:
         except EOFError:
             self.reconnect()
         except error, e:
+            print e
             self.reconnect()
             return None
 
@@ -233,7 +249,7 @@ class Boblight:
                 self.connect(self._host, self._port)
             except error, e:
                 pass
-        self.setPriority(self._priority)
+        self._sendPriority()
         self._sendInterpolation()
         self._sendSpeed()
         self._sendColor()
@@ -260,3 +276,6 @@ class Boblight:
 
     def getBrightness(self):
         return self._brightness
+
+    def _restartBoblightDaemon(self):
+        call("service boblightd restart", shell=True)
